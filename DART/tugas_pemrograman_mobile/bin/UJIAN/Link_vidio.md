20f8172bdf04d348368f5b47a76a92805566497d
Fauzan Mansur <fauzanmansyur806@gmail.com>
2025-10-26 17:01:53 +0900

    Update Tugas Pemrograman-Mobile

--- DART/tugas_pemrograman_mobile/bin/UJIAN/Link_vidio.md
+++ DART/tugas_pemrograman_mobile/bin/UJIAN/Link_vidio.md
@@ -1,10 +1,10 @@
 # TUGAS UJIAN TENGAH SEMESTER (UTS)
 
-- **Nama:** Nurdalifa Marzuki
-- **NPM:** 07352311166
+- **Nama:** Fauzan Mansur
+- **NPM:** 07352311180
 - **Kelas:** 5IF2
-- **Mata Kuliah:** Pemrograman Mobile 
+- **Mata Kuliah:** Pemrograman Mobile
 
 ## Link Video Live Coding
 Berikut ini merupakan link vidio saya:  
-👉 [https://youtu.be/FeHtYdlpY9M](https://youtu.be/FeHtYdlpY9M)
+👉 [https://youtu.be/hCdaeGFj3Us](https://youtu.be/hCdaeGFj3Us)
--- DART/tugas_pemrograman_mobile/bin/UJIAN/uts.dart
+++ DART/tugas_pemrograman_mobile/bin/UJIAN/uts.dart
@@ -1,70 +1,102 @@
 import 'dart:io';
+
+//=================
+//Kelas ABSTRAK
+//=================
 abstract class Transportasi {
   String id;
   String namaKendaraan;
   double _tarifDasar;
   int kapasitas;
-
+  //Method ==> Construct Agara Ketika Object dibuat Maka Properti akan Otomatis terpanggil
   Transportasi(this.id, this.namaKendaraan, this._tarifDasar, this.kapasitas);
+  //Method 1 ==> Encapsulasi
   double get tarifDasar => _tarifDasar;
+
+  //Method 2 ==> Menghitung Tarif Perjalanan
   double hitungTarif(int jumlahPenumpang);
+
+  //Method 3 ==> Menampilkan Informasi Kendaraan
   void tampilInfo() {
-    print("ID: $id");
-    print("Nama Kendaraan: $namaKendaraan");
-    print("Tarif Dasar: $_tarifDasar");
-    print("Kapasitas: $kapasitas");
+    print("ID : $id");
+    print("Nama Kendaraan : $namaKendaraan");
+    print("Tarif Dasar : $_tarifDasar");
+    print("Kapasitas : $kapasitas");
   }
 }
+
+//========================
+//Kelas Turunan (Taksi)
+//=======================
 class Taksi extends Transportasi {
-  double jarak; 
+  double jarak;
 
   Taksi(String id, String nama, double tarifDasar, int kapasitas, this.jarak)
-      : super(id, nama, tarifDasar, kapasitas);
+    : super(id, nama, tarifDasar, kapasitas);
 
-  @override
+  @override //Hitung Tarif Untuk Taksi
   double hitungTarif(int jumlahPenumpang) {
     return tarifDasar * jarak;
   }
 
-  @override
+  @override //Menampilkan Informasi Taksi
   void tampilInfo() {
     super.tampilInfo();
-    print("Jarak: $jarak km");
+    print("Jarak : $jarak Km");
   }
 }
+
+//==============================
+//Kelas Turunan (Bus)
+//==============================
 class Bus extends Transportasi {
   bool adaWifi;
-
   Bus(String id, String nama, double tarifDasar, int kapasitas, this.adaWifi)
-      : super(id, nama, tarifDasar, kapasitas);
+    : super(id, nama, tarifDasar, kapasitas);
 
-  @override
+  @override //Menghitung Tarif Bus
   double hitungTarif(int jumlahPenumpang) {
     return (tarifDasar * jumlahPenumpang) + (adaWifi ? 5000 : 0);
   }
-  @override
+
+  @override //Menampilkan Informasi Bus
   void tampilInfo() {
     super.tampilInfo();
-    print("Ada Wifi: ${adaWifi ? "Ya" : "Tidak"}");
+    print("Ada Wifi : ${adaWifi ? "Ya" : "Tidak"}");
   }
 }
-class Pesawat extends Transportasi {
-  String kelasPenerbangan; 
 
-  Pesawat(String id, String nama, double tarifDasar, int kapasitas, this.kelasPenerbangan)
-      : super(id, nama, tarifDasar, kapasitas);
+//================
+//Kelass Pesawat
+//================
+class Pesawat extends Transportasi {
+  String kelasPenerbangan;
+
+  Pesawat(
+    String id,
+    String nama,
+    double tarifDasar,
+    int kapasitas,
+    this.kelasPenerbangan,
+  ) : super(id, nama, tarifDasar, kapasitas);
 
   @override
   double hitungTarif(int jumlahPenumpang) {
-    return tarifDasar * jumlahPenumpang * (kelasPenerbangan == "Bisnis" ? 1.5 : 1.0);
+    return tarifDasar *
+        jumlahPenumpang *
+        (kelasPenerbangan == "Bisnis" ? 1.5 : 1.0);
   }
 
   @override
   void tampilInfo() {
     super.tampilInfo();
-    print("Kelas Penerbangan: $kelasPenerbangan");
+    print("Kelas Penerbangan : $kelasPenerbangan");
   }
 }
+
+//====================
+//Kelas Pemesanan
+//====================
 class Pemesanan {
   String idPemesanan;
   String namaPelanggan;
@@ -72,17 +104,21 @@ class Pemesanan {
   int jumlahPenumpang;
   double totalTarif;
 
-  Pemesanan(this.idPemesanan, this.namaPelanggan, this.transportasi,
-      this.jumlahPenumpang, this.totalTarif);
-
+  Pemesanan(
+    this.idPemesanan,
+    this.namaPelanggan,
+    this.transportasi,
+    this.jumlahPenumpang,
+    this.totalTarif,
+  );
   void cetakStruk() {
-    print("\n===== STRUK PEMESANAN =====");
-    print("ID Pemesanan: $idPemesanan");
-    print("Nama Pelanggan: $namaPelanggan");
-    print("Transportasi: ${transportasi.namaKendaraan}");
-    print("Jumlah Penumpang: $jumlahPenumpang");
-    print("Total Tarif: Rp$totalTarif");
-    print("============================\n");
+    print("\n================ Struk Pemesanan ================");
+    print("ID Pemesanan : $idPemesanan");
+    print("Nama Pelanggan : $namaPelanggan");
+    print("Transportasi : $transportasi");
+    print("Jumlah Penumpang : $jumlahPenumpang");
+    print("Total Tarif : Rp$totalTarif");
+    print("=================================================\n");
   }
 
   Map<String, dynamic> toMap() {
@@ -91,13 +127,20 @@ class Pemesanan {
       "nama": namaPelanggan,
       "transportasi": transportasi.namaKendaraan,
       "penumpang": jumlahPenumpang,
-      "total": totalTarif
+      "total": totalTarif,
     };
   }
 }
+
 Pemesanan buatPemesanan(Transportasi t, String nama, int jumlahPenumpang) {
   double total = t.hitungTarif(jumlahPenumpang);
-  return Pemesanan("P${DateTime.now().millisecondsSinceEpoch}", nama, t, jumlahPenumpang, total);
+  return Pemesanan(
+    "P${DateTime.now().millisecondsSinceEpoch}",
+    nama,
+    t,
+    jumlahPenumpang,
+    total,
+  );
 }
 
 void tampilSemuaPemesanan(List<Pemesanan> daftar) {
@@ -106,6 +149,10 @@ void tampilSemuaPemesanan(List<Pemesanan> daftar) {
     p.cetakStruk();
   }
 }
+
+//============================
+//FUNGSI UTAMA (MAIN)
+//============================
 void main() {
   // Membuat beberapa objek transportasi
   var taksi = Taksi("T1", "Blue Bird", 10000, 4, 15);
